5ebf9675eaa74072b31c6d7ef2e9e61c83296a81
Natalia Pear <gruscha.natascha2017@yandex.ru>
2023-07-31 08:45:24 +0300

    Изменила исходные данные

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT || 3001;
 const MONGO_DB = 'mongodb://0.0.0.0:27017/bitfilmsdb';
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV !== 'production'
 const JWT_SECRET = process.env.JWT_SECRET || '';
 
 module.exports = {
