8c4602ec878cea418681ca2ec2a15f438bc17a40
Natalia Pear <gruscha.natascha2017@yandex.ru>
2023-08-01 23:00:26 +0300

    Вернула подключение монги

--- app.config.js
+++ app.config.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 5010;
-const MONGO_DB = process.env.MONGO_DB || '';
+const MONGO_DB = 'mongodb://0.0.0.0:27017/bitfilmsdb';
 const NODE_ENV = process.env.NODE_ENV !== 'production';
 const JWT_SECRET = process.env.JWT_SECRET || 'production';
 
