ce033172ead0ab877fd15c288abb8f4ce78cc36b
Natalia Pear <gruscha.natascha2017@yandex.ru>
2023-08-01 23:16:15 +0300

    изменила порт

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 5010;
+const PORT = process.env.PORT || 3002;
 const MONGO_DB = 'mongodb://0.0.0.0:27017/bitfilmsdb';
 const NODE_ENV = process.env.NODE_ENV !== 'production';
 const JWT_SECRET = process.env.JWT_SECRET || 'production';
