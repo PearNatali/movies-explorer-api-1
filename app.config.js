6bc4e2516c1c5fda9469a6befcecfa9dbbe8eacc
Natalia Pear <gruscha.natascha2017@yandex.ru>
2023-08-01 22:37:13 +0300

    Вернула переменные окружения к изночальному виду

--- app.config.js
+++ app.config.js
@@ -1,12 +1,10 @@
-const PORT = process.env.PORT || 5005;
-const MONGO_NAME = process.env.MONGO_NAME || '';
+const PORT = process.env.PORT || 5010;
 const MONGO_DB = process.env.MONGO_DB || '';
 const NODE_ENV = process.env.NODE_ENV !== 'production';
 const JWT_SECRET = process.env.JWT_SECRET || 'production';
 
 module.exports = {
   PORT,
-  MONGO_NAME,
   MONGO_DB,
   NODE_ENV,
   JWT_SECRET,
